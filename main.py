a3907bd49dd244cc40fe5605ea827f891a5331a7
herrmalte <dahlen.christopher@gmail.com>
2023-11-12 23:29:07 +0100

    cleanup

--- main.py
+++ main.py
@@ -103,5 +103,3 @@ if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))  # Use PORT environment variable, default to 8000 if not set
     uvicorn.run(app, host="0.0.0.0", port=port)
-
-
